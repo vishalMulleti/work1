58b21cb62fcc0ea963b866ddcd26acaac6921932
Akshat Srivastava <akshat.srivastava.ug20@nsut.ac.in>
2023-05-29 05:44:17 +0530

    First Commit

--- client_test.py
+++ client_test.py
@@ -8,6 +8,7 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quotes[0]),('ABC',120.48,121.2,120.84))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +16,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quotes[0]),('ABC',120.48,119.2,119.84)) 
 
 
   """ ------------ Add more unit tests ------------ """
+  
 
 
 
