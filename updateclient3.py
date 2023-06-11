0e1f12a89eb606522825b49c546fa7a51010c05b
Armish11 <135872279+Armish11@users.noreply.github.com>
2023-06-11 18:44:10 +0500

    Update and rename client3.py to updateclient3.py

--- updateclient3.py
+++ updateclient3.py
@@ -25,24 +25,24 @@ import urllib.request
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
 
-# 500 server request
+# 500 server requests
 N = 500
 
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """Produce all the needed values to generate a data point"""
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Calculate average price
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    """Get the ratio of price_a and price_b"""
+    if price_b == 0:
+        return None  # Avoid division by zero error
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +51,12 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+            # Calculate the ratio
+            if stock == "XYZ":  # Replace "XYZ" with the desired stock symbol
+                ratio = getRatio(bid_price, ask_price)
+                if ratio is not None:
+                    print("Ratio %s" % ratio)
