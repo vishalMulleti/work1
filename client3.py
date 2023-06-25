6423e100110eb90741b0ce92983761ccf6a1a7f4
Keerthika <keerthikashetty08@gmail.com>
2023-06-25 19:45:00 +0530

    test2

--- client3.py
+++ client3.py
@@ -57,6 +57,7 @@ if __name__ == "__main__":
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
         print("Ratio %s" % getRatio(price, price))
+       # Asha you beauty
         
         
         
