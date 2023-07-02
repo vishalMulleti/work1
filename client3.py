a8267b999ce3da9ba2ba2f08421a7d8906fa6ff7
Mayank <mayankkashyap365@gmail.com>
2023-07-02 18:32:13 +0530

    Fixed Patch

--- client3.py
+++ client3.py
@@ -35,18 +35,17 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    try:
-        ratio = price_a / price_b
-    except ZeroDivisionError:
-        ratio = None
-    return ratio
+    if (price_b == 0):
+        return
+
+    return price_a / price_b;
 
 
 # Main
@@ -57,15 +56,11 @@ if __name__ == "__main__":
 
         """ ----------- Update to get the ratio --------------- """
 
-        prices = []
+        prices = {}
 
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        if len(prices) >= 2:
-            price_a = prices[0]
-            price_b = prices[1]
-            ratio = getRatio(price_a , price_b)
-            if ratio is not None:
-                print("Ratio %s" % ratio)
+        print("Ratio %s" % getRatio(prices["ABC"] , prices["DEF"]))
