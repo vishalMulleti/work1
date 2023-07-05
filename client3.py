d52c86c857dd84fd588fbad4ba9042ec93183828
Zweli528 <123547274+Zweli528@users.noreply.github.com>
2023-07-05 07:56:56 +0200

    Update client3.py

--- client3.py
+++ client3.py
@@ -43,13 +43,11 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     """ Also create some unit tests for this function in client_test.py """
-
-    return 1
     
-    if (price_b==0):
-              return
+    if(price_b==0):
+           # when price_b is zero avoid throwing ZeroDivisionError
+            return
     return price_a/price_b
-        
 
 
 # Main
@@ -59,8 +57,11 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
+       
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
