5c5b55672c8b83949b396fc698192362ff9d065a
davidemusi <davidemusi4gospel@gmail.com>
2023-10-11 17:51:28 +0100

    Update clients.py and client_test.py

--- client3.py
+++ client3.py
@@ -28,10 +28,13 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
+# variables to store the last prices of two stocks
+last_price_a = None
+last_price_b = None
+
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
@@ -41,19 +44,33 @@ def getDataPoint(quote):
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b is not None and price_b != 0:
+        return price_a / price_b
+    return None
 
 
 # Main
-if __name__ == "__main__":
+if __name__ == "__main":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
-        """ ----------- Update to get the ratio --------------- """
+        
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            if stock == 'AAPL':
+                price_a = price
+            elif stock == 'MSFT':
+                price_b = price
 
-        print("Ratio %s" % getRatio(price, price))
+        # Check if we have both prices
+        if price_a is not None and price_b is not None:
+            price_ratio = getRatio(price_a, price_b)
+            if price_ratio is not None:
+                print("Quoted AAPL at (bid:%s, ask:%s, price:%s)" % (bid_price, ask_price, price_a))
+                print("Quoted MSFT at (bid:%s, ask:%s, price:%s)" % (bid_price, ask_price, price_b))
+                print("Ratio %s" % price_ratio)
+        else:
+            print("Missing price data for one or both stocks")
+
+        last_price_a = price_a
+        last_price_b = price_b
