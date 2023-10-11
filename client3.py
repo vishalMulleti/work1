3ee9e774f5d920a0f33563771ea065f95ef37177
Sourcery AI <>
2023-10-11 08:03:12 +0000

    'Refactored by Sourcery'

--- client3.py
+++ client3.py
@@ -36,7 +36,7 @@ def getDataPoint(quote):
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
     price = bid_price
-    return stock, bid_price, ask_price, price
+    return stock, price, ask_price, price
 
 
 def getRatio(price_a, price_b):
@@ -54,6 +54,6 @@ if __name__ == "__main__":
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:{price})")
 
-        print("Ratio %s" % getRatio(price, price))
+        print(f"Ratio {getRatio(price, price)}")
