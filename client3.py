10be41db018eb42c71aafa3a2bd186484ab408d5
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-17 17:06:33 -0500

    Delete redundant comments

--- client3.py
+++ client3.py
@@ -40,7 +40,6 @@ def getDataPoint(quote):
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
     if (price_b == 0):
         # When price_b is 0, avoid throwing ZeroDivisionError
         return
