672d11f5dc74fc259a9b0daef59218b6749f4e25
udaysom2120 <uday.som19@st.niituniversity.in>
2023-11-23 12:53:05 +0530

    Updated Unit tests

--- client_test.py
+++ client_test.py
@@ -7,7 +7,14 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+
+    '''ACT'''
+    result = getDataPoint(quotes)
+
     """ ------------ Add the assertion below ------------ """
+    expected_price = (120.48 + 121.2) / 2
+    self.assertEqual(result[3], expected_price)
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,6 +22,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    #Act
+    result = getDataPoint(quotes)
+
+    #Assert
+    expected_price = (119.2 + 120.48) / 2
+    self.assertEqual(result[3], expected_price)
 
 
   """ ------------ Add more unit tests ------------ """
