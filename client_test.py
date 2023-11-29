9bc00be5976f8f2ab80c1f4551d17a5b5778e2e3
Evin-Ngoa <toneevin07@gmail.com>
2023-11-29 17:45:06 +0300

    Task 1 with patch file completed

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      self.assertEqual(price, (bid_price + ask_price) / 2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +18,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      self.assertEqual(price, (bid_price + ask_price) / 2)
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_validPrices(self):
+      self.assertEqual(getRatio(10, 5), 2.0)
+      self.assertEqual(getRatio(5, 10), 0.5)
+
+  def test_getRatio_zeroPrice(self):
+      self.assertIsNone(getRatio(0, 10))
+      self.assertIsNone(getRatio(10, 0))
+
+  def test_getRatio_bothZeroPrices(self):
+      self.assertIsNone(getRatio(0, 0))
 
 
 
