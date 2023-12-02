a040a93b171bd8319d3dad1b4c0d4bacae1a1d98
vishnu1972 <144895277+vishnu1972@users.noreply.github.com>
2023-12-02 10:32:02 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -25,24 +25,28 @@ import urllib.request
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
 
-# 500 server request
+# 500 server requests
 N = 500
+
+# Dictionary to store stock prices
+prices = {}
 
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Calculate average of bid and ask prices
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        # Avoid division by zero
+        return None
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +55,20 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # Iterate through quotes to get stock info and prices
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+
+            # Store the datapoint in the prices dictionary
+            prices[stock] = price
+
+            # Print stock info and prices
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # Calculate and print the ratio of the two stock prices
+        if 'stockA' in prices and 'stockB' in prices:
+            ratio = getRatio(prices['stockA'], prices['stockB'])
+            if ratio is not None:
+                print("Ratio %s" % ratio)
+            else:
+                print("Cannot calculate ratio, one of the prices is zero.")
