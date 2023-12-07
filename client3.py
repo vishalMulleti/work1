edb9515757fb130d2fa10ccb4d6a4fd526a04830
Bhavit <84284964+bhavit069@users.noreply.github.com>
2023-12-07 22:59:14 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -35,14 +35,18 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    # - price = bid_price
+    price = (bid_price+ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    # should be returning price_a/price_b
+    if (price_b == 0): # this is because mathematically, anything divided by 0 is undefined. This is a RARE case
+        return
+    return price_a/price_b
 
 
 # Main
@@ -51,9 +55,12 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
+        prices = {} # dictionary to store data
+
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
