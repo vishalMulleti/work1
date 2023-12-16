a9c55f21ac3367a95fc01f50026ec73c6f767ec4
warenya <warenyaranmini123@gmail.com>
2023-12-16 14:08:54 +0530

    patch 1 with unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,7 @@
 import unittest
-from client3 import getDataPoint
+import self
+from client3 import getDataPoint, quotes
+
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +10,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), {quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2})
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +19,14 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
-
   """ ------------ Add more unit tests ------------ """
+    quotes = [
+      {'top_ask': {'price': 100.5, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 115.5, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 105.2, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 200.7, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
 
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), {quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2})
 
 if __name__ == '__main__':
     unittest.main()
