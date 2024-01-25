11cc50f36b0e9540a4f475810ec6b5fe38598cf8
Raymond.Sarpong <137334676+RaymondSarpong@users.noreply.github.com>
2024-01-25 10:36:00 +0000

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -7,7 +7,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    for in in quotes:
+      self.assertEqual(getDataPoint(i),i["stock"],i["bid_price"],i["ask_price"],(i["bid_price"]+i["ask_price"])/2))
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
