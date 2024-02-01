70e8ac8da31f7512795af01a8925bcc694d233ca
saimitra <saimithra11@gmail.com>
2024-02-01 05:14:20 -0500

    Completed the client3.py modifications as per task requirements

--- client3.py
+++ client3.py
@@ -21,6 +21,8 @@
 import json
 import random
 import urllib.request
+import time
+
 
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
@@ -35,25 +37,29 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return 0
+    return price_a / price_b
 
 
+
+# Main
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # Process the quotes
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # Wait for N seconds before querying again
+        time.sleep(N)  # This will pause the execution for N seconds
+
