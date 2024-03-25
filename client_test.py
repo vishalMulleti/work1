8f8b69ec3ec8b945c10fefd709218d590c8547e6
WillAdebisi <152137039+WillAdebisi@users.noreply.github.com>
2024-03-24 20:37:51 -0500

    "Implemented getDataPoint function to ensure the accurate retrieval of stock information including stock name, bid price, ask price, and price (calculated as the average of bid and ask prices).
    Updated getRatio function to correctly calculate the ratio between two stock prices.
    
    Refined main function to output precise stock information, bid and ask prices, and the corresponding price ratio."
    
    Created different test case to guarantee edits were properly made

--- client_test.py
+++ client_test.py
@@ -1,6 +1,6 @@
 import unittest
 from client3 import getDataPoint
-
+from client3 import getRatio
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
     quotes = [
@@ -23,3 +23,31 @@ class ClientTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+class TestYourFunctions(unittest.TestCase):
+
+  # Test when price_b = 0
+
+  def test_divide_by_zero(self):
+    price_a = 7
+    price_b = 0
+    self.assertIsNone(getRatio(price_a, price_b))
+
+#Tests If price_a is equal to zero
+def test_a_is_zero(self):
+    price_a = 0
+    price_b = 8
+    self.assertIsEqual(getRatio(price_a, price_b),0)
+
+#Tests for normal inputs
+def normal_case(self):
+  price_a = 200
+  price_b = 10
+  self.assertIsEqual(getRatio(price_a, price_b), 20)
+
+#When neither variable are zero and price_a is a float
+def float_case(self):
+  price_a = 200.5
+  price_b = 10
+  self.assertIsEqual(getRatio(price_a, price_b), 20.05)
+
