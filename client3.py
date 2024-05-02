11c3ad39c050e5adb81fb44e2fc66d570edd2d68
Sabitha-Devaraj <vdsabitha@gmail.com>
2024-05-03 00:16:34 +0100

    Update client3.py

--- client3.py
+++ client3.py
@@ -42,6 +42,7 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
+   
     if(price_b == 0):
         return
     return price_a/price_b
