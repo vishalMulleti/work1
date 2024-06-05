d1181ba958be294257877d73f12b0bb023d2b305
Madhan Maddigalla <139124164+Maddigallamadhan@users.noreply.github.com>
2024-06-05 08:00:47 +0530

    Update client3_test.py

--- client_test.py
+++ client_test.py
@@ -1,25 +1,7 @@
-import unittest
-from client3 import getDataPoint
-
-class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
-
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
-
-
-  """ ------------ Add more unit tests ------------ """
-
-
-
-if __name__ == '__main__':
-    unittest.main()
+def getDataPoint(quote):
+    """ Produce all of the needed values to generate a datapoint """
+    stock = quote['stock']
+    bid_price = quote['top_bid']['price']
+    ask_price = quote['top_ask']['price']
+    price = (bid_price + ask_price) / 2
+    return stock, bid_price, ask_price, price
