1d7c2b5ce75e52e41290812ffd6a7527d0ebaac0
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-07 16:06:26 +0300

    updated client3.py file and client_test.py file

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,24 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_positive_numbers(self):
+        self.assertEqual(getRatio(100.0, 50.0), 2.0)
+        self.assertEqual(getRatio(75.0, 25.0), 3.0)
+
+  def test_getRatio_zero_denominator(self):
+        self.assertIsNone(getRatio(100.0, 0.0))
+
+  def test_getRatio_negative_numbers(self):
+        self.assertEqual(getRatio(-100.0, -50.0), 2.0)
+        self.assertEqual(getRatio(-75.0, -25.0), 3.0)
+
+  def test_getRatio_same_numbers(self):
+        self.assertEqual(getRatio(100.0, 100.0), 1.0)
 
 
 
