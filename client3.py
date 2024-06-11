17dee196ff8823f0ad8df255e8da7e579458a501
Nestor Martourez <nestorabiawuh@gmail.com>
2024-06-11 13:11:49 +0300

    did the adjustments int the client3 and server3 files

--- client3.py
+++ client3.py
@@ -31,17 +31,15 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price+ask_price)/2
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
     if price_b == 0:
         return
     return price_a / price_b
--- client_test.py
+++ client_test.py
@@ -17,20 +17,20 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
-  def test_getRatio_calculation_for_different_prices(result):
+  def test_getRatio_calculation_for_different_prices(self):
     """Tests that getRatio returns the correct ratio for different prices."""
 
     # Test with positive prices
-    result = getRatio(10, 5)
-    assert result == 2.0, "Ratio of positive prices is incorrect"
+    self = getRatio(10, 5)
+    assert self == 2.0, "Ratio of positive prices is incorrect"
 
     # Test with negative prices
-    result = getRatio(-3, 2)
-    assert result == -1.5, "Ratio of negative prices is incorrect"
+    self = getRatio(-3, 2)
+    assert self == -1.5, "Ratio of negative prices is incorrect"
 
     # Test with zero as the second price
-    result = getRatio(12, 0)
-    assert result is None, "Ratio shouldn't be calculated when price_b is zero"
+    self = getRatio(12, 0)
+    assert self is None, "Ratio shouldn't be calculated when price_b is zero"
 
   """ ------------ Add more unit tests ------------ """
 
